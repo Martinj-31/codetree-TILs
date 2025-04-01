0b43bced769b81685487a27873e1adb1e8748613
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2025-04-01 17:16:02 +0900

    Title: 자율주행 자동차, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250401/자율주행 자동차/autonomous-driving.py
@@ -0,0 +1,28 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+x, y, d = map(int, sys.stdin.readline().split())
+road = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+dir = [0, 1, 2, 3]
+
+
+cnt = 0
+while True:
+    for _ in range(4):
+        d = dir[d - 1]
+        nx, ny = x + dx[d], y + dy[d]
+        if road[nx][ny] == 0:
+            cnt += 1
+            road[nx][ny] = 2
+            x, y = nx, ny
+            break
+    else:
+        d = dir[d - 3]
+        nx, ny = x + dx[d], y + dy[d]
+        if road[nx][ny] == 1:
+            break
+
+print(cnt)
