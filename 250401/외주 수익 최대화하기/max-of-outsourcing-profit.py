02e256e4967e5f5a23524213dee70ffc675abdcd
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2025-04-01 18:11:14 +0900

    Title: 외주 수익 최대화하기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250401/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+work = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+worked = [0 for _ in range(len(work))]
+
+
+def dfs(t, p):
+    global result
+    if t >= n:
+        result = max(result, p)
+        return
+
+    dfs(t + work[t][0], p + work[t][1])
+
+
+result = 0
+for i in range(len(work)):
+    dfs(i + work[i][0], work[i][1])
+print(result)
